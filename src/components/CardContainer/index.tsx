798c417f39717c776192fbe57805a66a6ee12dfe
diozenio <d.dionisiobsl@gmail.com>
2025-07-09 21:38:09 -0300

    Implement transaction summary in CardContainer component

--- src/components/CardContainer/index.tsx
+++ src/components/CardContainer/index.tsx
@@ -1,11 +1,29 @@
+"use client";
+import { useTransactions } from "@/hooks/useTransactions";
 import { Card } from "../Card";
 
-export function CardContainer(){
-    return (
-        <div className="flex justify-between">
-          <Card title="Entradas" value={100} type="income" />
-          <Card title="Entradas" value={100} type="outcome" />
-          <Card title="Entradas" value={100} type="total" />
-        </div>
-    )
+export function CardContainer() {
+  const { transactions } = useTransactions();
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "income") {
+        acc.income += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.outcome += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+      return acc;
+    },
+    { income: 0, outcome: 0, total: 0 }
+  );
+
+  return (
+    <div className="flex justify-between">
+      <Card title="Entradas" value={summary.income} type="income" />
+      <Card title="Saídas" value={summary.outcome} type="outcome" />
+      <Card title="Total" value={summary.total} type="total" />
+    </div>
+  );
 }
